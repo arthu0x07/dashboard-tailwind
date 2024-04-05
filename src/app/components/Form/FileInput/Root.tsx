fd4c610184d774c002151250598d911ac4e6ca8a
Arthur Silva <arthur.santos.off@gmail.com>
2024-04-05 00:34:33 -0300

    feat: use a context to fix the cross-form-click issue and create a files preview

--- src/app/components/Form/FileInput/Root.tsx
+++ src/app/components/Form/FileInput/Root.tsx
@@ -1,7 +1,34 @@
-import { ComponentProps } from 'react'
+'use client'
+
+import {
+  ComponentProps,
+  createContext,
+  useContext,
+  useId,
+  useState,
+} from 'react'
 
 export interface Root extends ComponentProps<'div'> {}
 
-export function Root(props: Root) {
-  return <div {...props}></div>
+type FileInputContextType = {
+  id: string
+  files: File[]
+  onFilesSelected: (files: File[]) => void
 }
+
+const FileInputContext = createContext({} as FileInputContextType)
+
+export function Root(props: Root) {
+  const id = useId()
+  const [files, setFiles] = useState<File[]>([])
+
+  console.log(files)
+
+  return (
+    <FileInputContext.Provider value={{ id, files, onFilesSelected: setFiles }}>
+      <div {...props}></div>
+    </FileInputContext.Provider>
+  )
+}
+
+export const useFileInput = () => useContext(FileInputContext)
