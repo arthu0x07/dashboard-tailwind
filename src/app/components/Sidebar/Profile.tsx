6c15be7cd8aa99329ef5fb28bed8f608238db537
Arthur Silva <arthur.santos.off@gmail.com>
2024-04-06 21:40:46 -0300

    feat: implement a sidebar dark theme

--- src/app/components/Sidebar/Profile.tsx
+++ src/app/components/Sidebar/Profile.tsx
@@ -10,17 +10,17 @@ export function Profile() {
       />
 
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Arthur Silva
         </span>
 
-        <span className="truncate text-sm text-zinc-500 ">
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           arthur.santos.off@gmail.com
         </span>
       </div>
 
       <Button type="button" variant="ghost">
-        <LogOut className="h-5 w-5 text-zinc-500" />
+        <LogOut className="h-5 w-5 text-zinc-500 " />
       </Button>
     </div>
   )
